3f1ef04dbd341cefd7db9ccd6572567dfc13a097
Soster <agustinsoster@gmail.com>
2026-09-14 00:24:30 -0300

    distinct, where y order by

---
+++ statements/04_order_by.sql
@@ -0,0 +1,4 @@
+SELECT * FROM users ORDER BY age;
+SELECT * FROM users ORDER BY age ASC; #El ASC es ascendente
+SELECT * FROM users ORDER BY age DESC; #El DESC es descendente
+SELECT * FROM users WHERE email = 'agustinsoster@gmail.com' ORDER BY age DESC;
