752fb041d7b555ca40075c5e28f6715945b33a46
Soster <agustinsoster@gmail.com>
2026-09-15 00:13:45 -0300

    like, and, or, not, limit

--- statements/03_WHERE.sql
+++ statements/03_WHERE.sql
@@ -1,3 +1,3 @@
 SELECT * FROM users WHERE age = 27;
 SELECT * FROM users WHERE age > 27;
-SELECT DISTINCT name FROM users WHERE age = 27 #combinacion de varios comandos, en este caso distinguimos dos nombres distintos (con el distinct) de personas con la misma edad
+SELECT DISTINCT name FROM users WHERE age = 27; #combinacion de varios comandos, en este caso distinguimos dos nombres distintos (con el distinct) de personas con la misma edad
